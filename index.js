7a1088d7ab74c31cf3b9c91c19300f83f1f56230
Ahmd Mufahras Li Alfazh Assardew <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-05-27 11:26:52 +0700

    feat: upload file function and reset button

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+const container = document.getElementById("the-chosen");
+const theChosenOne = document.createElement("h3");
+
 function getInput() {
     const input = document.getElementById("name").value;
+    const inputfile = document.getElementById("inputFile").value;
+    const textVar = input.split("\n").join(",").split(" ").join(",").split(",");
+    const lengthList = textVar.length;
+    const randNumber = Math.ceil(Math.random() * lengthList) - 1;
+    console.log(inputfile);
+    console.log(randNumber);
+    console.log(lengthList);
+    console.log(textVar);
     console.log(input);
+
+    theChosenOne.id = "the-chosen-one";
+    theChosenOne.innerText = "the chosen One is " + textVar[randNumber];
+
+    container.append(theChosenOne);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -8,5 +24,34 @@ document.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (event) {
         event.preventDefault();
         getInput();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const inputFile = document.getElementById("inputFile");
+    inputFile.addEventListener("change", function (event) {
+        event.preventDefault();
+        const file = event.target.files[0];
+        console.log(file);
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function (e) {
+            const text = e.target.result;
+            document.getElementById("name").value = text;
+        };
+    });
+});
+
+function removeChosen() {
+    const theChosenOne = document.getElementById("the-chosen-one");
+    theChosenOne.remove();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const resetButton = document.getElementById("resetButton");
+    resetButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        document.getElementById("name").value = "";
+        removeChosen();
     });
 });
